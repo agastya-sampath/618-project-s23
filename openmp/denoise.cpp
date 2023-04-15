2f9f31407c1f1aae646f09974516b232749a0ffa
Agastya Sampath <agastyas@ghc74.ghc.andrew.cmu.edu>
2023-04-15 16:51:20 -0400

    Refactor MF denoising code, comments

--- openmp/denoise.cpp
+++ openmp/denoise.cpp
@@ -17,57 +17,66 @@ void OMPMedianFilterDenoise(CImg<unsigned char> &orig, CImg<unsigned char> &res,
     // 1 channel + 3 colors + 0 prefilled
     res.assign(orig.width() - 2 * mid, orig.height() - 2 * mid, 1, 3, 0);
 
-    #pragma omp parallel for schedule(dynamic, 8)
-    for (int i = mid; i < orig.height() - mid; i++)
+#pragma omp parallel for schedule(dynamic, 8)
+    for (int i = mid; i < orig.height() - mid; i++) // No padding
     {
-        #pragma omp parallel for schedule(dynamic, 8)
-        for (int j = mid; j < orig.width() - mid; j++)
+#pragma omp parallel for schedule(dynamic, 8)
+        for (int j = mid; j < orig.width() - mid; j++) // No padding
         {
             std::vector<rgb_t> rgbNeighbors;
 
-            rgb_t rgbAccumulate(0.0f, 0.0f, 0.0f); 
+            rgb_t rgbAccumulate(0.0f, 0.0f, 0.0f);
 
-            for (int x = i - mid; x <= i + mid; x++)
+            // Find neighbors of current pixel within filter range
+            for (int x = -mid; x <= mid; x++)
             {
-                for (int y = j - mid; y <= j + mid; y++)
+                for (int y = -mid; y <= mid; y++)
                 {
-                    rgbNeighbors.push_back(rgb_t(orig(y, x, 0, 0), orig(y, x, 0, 1), orig(y, x, 0, 2)));
+                    rgbNeighbors.push_back(rgb_t(orig(j + y, i + x, 0, 0), orig(j + y, i + x, 0, 1), orig(j + y, i + x, 0, 2)));
                 }
             }
 
+            // Find median pixel
             int median = (k % 2) ? (k * k - 1) / 2 : k * k / 2;
 
-            int n_elements = (k * k) / 2 * (int(perc) / 100);
+            // Number of neighbors (filter size * the percentage scaling)
+            int n_neighbors = (k * k) / 2 * (int(perc) / 100);
 
-            for (int colors = 0; colors < 3; colors++) {
-                if (colors == 0) {
-                    std::sort(rgbNeighbors.begin(), rgbNeighbors.end(), compareR);
-                    for (int idx = (median - n_elements); idx <= (median + n_elements); idx++)
-                    {
-                        rgbAccumulate.r += rgbNeighbors[idx].r;
-                    }
-                }
-                else if (colors == 1) {
-                    std::sort(rgbNeighbors.begin(), rgbNeighbors.end(), compareG);
-                    for (int idx = (median - n_elements); idx <= (median + n_elements); idx++)
-                    {
-                        rgbAccumulate.g += rgbNeighbors[idx].g;
-                    }
-                }
-                else {
-                    std::sort(rgbNeighbors.begin(), rgbNeighbors.end(), compareB);
-                    for (int idx = (median - n_elements); idx <= (median + n_elements); idx++)
-                    {
-                        rgbAccumulate.b += rgbNeighbors[idx].b;
-                    }
-                }
-            }
-
-            if (n_elements >= 1)
+            for (int colors = 0; colors < 3; colors++)
             {
-                rgbAccumulate *= rgb_t(1./float(2 * n_elements + 1), 1./float(2 * n_elements + 1), 1./float(2 * n_elements + 1));
+                // Sort r values
+                if (colors == 0)
+                {
+                    std::sort(rgbNeighbors.begin(), rgbNeighbors.end(), compareR);
+                    for (int idx = -n_neighbors; idx <= n_neighbors; idx++)
+                    {
+                        rgbAccumulate.r += rgbNeighbors[median + idx].r;
+                    }
+                }
+                // Sort g values
+                else if (colors == 1)
+                {
+                    std::sort(rgbNeighbors.begin(), rgbNeighbors.end(), compareG);
+                    for (int idx = -n_neighbors; idx <= n_neighbors; idx++)
+                    {
+                        rgbAccumulate.g += rgbNeighbors[median + idx].g;
+                    }
+                }
+                // Sort b values
+                else
+                {
+                    std::sort(rgbNeighbors.begin(), rgbNeighbors.end(), compareB);
+                    for (int idx = -n_neighbors; idx <= n_neighbors; idx++)
+                    {
+                        rgbAccumulate.b += rgbNeighbors[median + idx].b;
+                    }
+                }
             }
 
+            // Average
+            rgbAccumulate *= rgb_t(1. / float(2 * n_neighbors + 1), 1. / float(2 * n_neighbors + 1), 1. / float(2 * n_neighbors + 1));
+
+            // Assign to result
             res(j - mid, i - mid, 0, 0) = rgbAccumulate.r;
             res(j - mid, i - mid, 0, 1) = rgbAccumulate.g;
             res(j - mid, i - mid, 0, 2) = rgbAccumulate.b;
@@ -83,7 +92,7 @@ int main()
     Timer TotalSimulationTimer;
     OMPMedianFilterDenoise(orig, res, 5, 50); // Apply denoising with 5x5 kernel and 50% percile
     float totalSimulationTime = TotalSimulationTimer.elapsed();
-    std::cout << "Total simulation time: "<< totalSimulationTime << std::endl;
+    std::cout << "Total simulation time: " << totalSimulationTime << std::endl;
 
     res.save("output.png"); // Save output image
     return 0;
