298a072af7a2ca66daf1d14ef09b361ed096991b
BrandonBian <shijie.brandon.bian@gmail.com>
2023-05-01 15:42:54 -0400

    CUDA ycbcr -> RGB working

--- cuda/cuda.cu
+++ cuda/cuda.cu
@@ -60,3 +60,48 @@ void CUDARGBtoYCbCr(const unsigned char *img, unsigned char *res, int width, int
     cudaFree(d_input);
     cudaFree(d_output);
 }
+
+__global__ void YCbCrtoRGBKernel(const unsigned char *img, unsigned char *res, const int width, const int height)
+{
+    const int x = blockIdx.x * blockDim.x + threadIdx.x;
+    const int y = blockIdx.y * blockDim.y + threadIdx.y;
+    if (x >= width || y >= height)
+        return;
+
+    // Ref: https://cimg.eu/reference/storage.html
+    const unsigned char Y = img[y * width + x];
+    const unsigned char Cb = img[(height + y) * width + x];
+    const unsigned char Cr = img[(height * 2 + y) * width + x];
+
+    // Perform YCbCr to RGB conversion
+    const double R = Y + 1.402 * (Cr - 128);
+    const double G = Y - 0.344136 * (Cb - 128) - 0.714136 * (Cr - 128);
+    const double B = Y + 1.772 * (Cb - 128);
+
+    // Store the converted values back into the image
+    res[y * width + x] = (unsigned char)R;
+    res[(height + y) * width + x] = (unsigned char)G;
+    res[(height * 2 + y) * width + x] = (unsigned char)B;
+}
+
+void CUDAYCbCrtoRGB(const unsigned char *img, unsigned char *res, int width, int height)
+{
+    const int w = width;
+    const int h = height;
+    const int size = w * h * 3 * sizeof(unsigned char);
+
+    unsigned char *d_input, *d_output;
+    cudaMalloc((void **)&d_input, size);
+    cudaMalloc((void **)&d_output, size);
+    cudaMemcpy(d_input, img, size, cudaMemcpyHostToDevice);
+
+    dim3 block(16, 16);
+    dim3 grid((w + block.x - 1) / block.x, (h + block.y - 1) / block.y);
+
+    YCbCrtoRGBKernel<<<grid, block>>>(d_input, d_output, w, h);
+
+    cudaMemcpy(res, d_output, size, cudaMemcpyDeviceToHost);
+
+    cudaFree(d_input);
+    cudaFree(d_output);
+}
--- cuda/main.cpp
+++ cuda/main.cpp
@@ -11,15 +11,64 @@ using namespace cimg_library;
 
 // Conversion
 void CUDARGBtoYCbCr(const unsigned char *img, unsigned char *res, int width, int height);
+void CUDAYCbCrtoRGB(const unsigned char *img, unsigned char *res, int width, int height);
 
 int main()
 {
-    CImg<unsigned char> inputImage("images-input/inputConversion.bmp");
+    /////////////////////
+    /* - Parse Input - */
+    /////////////////////
 
-    // create an empty output image with 3 channels
-    CImg<unsigned char> outputImage(inputImage.width(), inputImage.height(), 1, 3);
+    std::string functionality;
+    std::string algorithm;
+    std::string inputPath;
+    std::string outputPath;
 
-    CUDARGBtoYCbCr(inputImage, outputImage, inputImage.width(), inputImage.height());
+    std::cout << "Enter functionality (TODO/TODO/conversion): ";
+    std::cin >> functionality;
 
-    outputImage.save("conversion-ycbcr.png");
+    if (functionality == "conversion")
+    {
+        ;
+    }
+    else
+    {
+        std::cerr << "Invalid Functionality Selection" << std::endl;
+        return 1;
+    }
+
+    std::cout << "Enter input image path: ";
+    std::cin >> inputPath;
+
+    ///////////////////////
+    /* - Run Algorithm - */
+    ///////////////////////
+
+    CImg<unsigned char> inputImage(inputPath.c_str());
+    CImg<unsigned char> outputImage;
+    Timer MyTimer;
+
+    if (functionality == "conversion")
+    {
+        ////////////////////
+        /* - Conversion - */
+        ////////////////////
+
+        // RGB to YCBCR + Grayscale
+        CImg<unsigned char> resgray(inputImage.width(), inputImage.height(), 1, 3);
+        CImg<unsigned char> resrgb(inputImage.width(), inputImage.height(), 1, 3);
+        CUDARGBtoYCbCr(inputImage, resgray, inputImage.width(), inputImage.height());
+
+        float duration = MyTimer.elapsed();
+        std::cout << "YCBCR/Grayscale simulation time: " << duration << std::endl;
+        resgray.save("images-output/conversion-ycbcr.png");
+        resgray.get_channel(0).save("images-output/conversion-grayscale.png");
+
+        // YCBCR to RGB
+        Timer RGBSimulationTimer;
+        CUDAYCbCrtoRGB(resgray, resrgb, resgray.width(), resgray.height());
+        float rgbSimulationTime = RGBSimulationTimer.elapsed();
+        std::cout << "RGB simulation time: " << rgbSimulationTime << std::endl;
+        resrgb.save("images-output/conversion-rgb.png");
+    }
 }
