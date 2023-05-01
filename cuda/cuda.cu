fdc55ee680dae7837ce21079e105d345153a556d
BrandonBian <shijie.brandon.bian@gmail.com>
2023-05-01 17:44:15 -0400

    finished cuda histogram equalization

--- cuda/cuda.cu
+++ cuda/cuda.cu
@@ -12,6 +12,121 @@
 #include <thrust/device_malloc.h>
 #include <thrust/device_free.h>
 
+////////////////////////////////
+/* - Enhance (Equalization) - */
+////////////////////////////////
+
+__global__ void histogramKernel(const unsigned char *d_img, int *d_hist_r, int *d_hist_g, int *d_hist_b, int width, int height)
+{
+    // Calculate histogram for each color channel
+    // [d_hist_r == hist_r]
+    // [d_hist_g == hist_g]
+    // [d_hist_b == hist_b]
+
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (x < width && y < height)
+    {
+        int offset = y * width + x;
+        atomicAdd(&d_hist_r[d_img[offset]], 1);
+        atomicAdd(&d_hist_g[d_img[offset + width * height]], 1);
+        atomicAdd(&d_hist_b[d_img[offset + 2 * width * height]], 1);
+    }
+}
+
+__global__ void equalizationKernel(const unsigned char *d_img, unsigned char *d_res, float *d_cum_hist, int width, int height)
+{
+    // Apply histogram equalization to each color channel
+    // [d_res == res]
+    int x = blockIdx.x * blockDim.x + threadIdx.x;
+    int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+    if (x < width && y < height)
+    {
+        int offset = y * width + x;
+        d_res[offset] = (unsigned char)(d_cum_hist[d_img[offset]] * 255.0f);
+        d_res[offset + width * height] = (unsigned char)(d_cum_hist[d_img[offset + width * height]] * 255.0f);
+        d_res[offset + 2 * width * height] = (unsigned char)(d_cum_hist[d_img[offset + 2 * width * height]] * 255.0f);
+    }
+}
+
+__global__ void compute_hist_avg(const int *d_hist_r, const int *d_hist_g, const int *d_hist_b,
+                                 const int total_pixels, const int bins, float *d_hist_avg)
+{
+    int i = blockIdx.x * blockDim.x + threadIdx.x;
+
+    if (i < bins)
+    {
+        d_hist_avg[i] = (d_hist_r[i] + d_hist_g[i] + d_hist_b[i]) / (3.0f * total_pixels);
+    }
+}
+
+void CUDAHistogramEqualization(const unsigned char *img, unsigned char *res, const int width, const int height)
+{
+    const int bins = 256;
+
+    // Allocate device memory
+    unsigned char *d_img, *d_res;
+    cudaMalloc((void **)&d_img, width * height * 3 * sizeof(unsigned char));
+    cudaMalloc((void **)&d_res, width * height * 3 * sizeof(unsigned char));
+    cudaMemcpy(d_img, img, width * height * 3 * sizeof(unsigned char), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_res, res, width * height * 3 * sizeof(unsigned char), cudaMemcpyHostToDevice);
+
+    int *d_hist_r, *d_hist_g, *d_hist_b;
+    cudaMalloc((void **)&d_hist_r, bins * sizeof(int));
+    cudaMalloc((void **)&d_hist_g, bins * sizeof(int));
+    cudaMalloc((void **)&d_hist_b, bins * sizeof(int));
+    cudaMemset(d_hist_r, 0, bins * sizeof(int));
+    cudaMemset(d_hist_g, 0, bins * sizeof(int));
+    cudaMemset(d_hist_b, 0, bins * sizeof(int));
+
+    float *d_hist_avg;
+    cudaMalloc(&d_hist_avg, bins * sizeof(float));
+    cudaMemset(d_hist_avg, 0, bins * sizeof(float));
+
+    float *d_cum_hist;
+    cudaMalloc(&d_cum_hist, bins * sizeof(float));
+    cudaMemset(d_cum_hist, 0, bins * sizeof(float));
+
+    // Compute histograms
+    dim3 blockDims(16, 16);
+    dim3 gridDims((width + blockDims.x - 1) / blockDims.x, (height + blockDims.y - 1) / blockDims.y);
+
+    histogramKernel<<<gridDims, blockDims>>>(d_img, d_hist_r, d_hist_g, d_hist_b, width, height);
+    cudaDeviceSynchronize();
+
+    // Compute average histogram
+    int total_pixels = width * height;
+    compute_hist_avg<<<gridDims, blockDims>>>(d_hist_r, d_hist_g, d_hist_b, total_pixels, bins, d_hist_avg);
+
+    // Compute cumulative histogram
+    float hist_avg[bins] = {0};
+    cudaMemcpy(hist_avg, d_hist_avg, bins * sizeof(float), cudaMemcpyDeviceToHost);
+
+    float cum_hist[bins] = {0};
+    cum_hist[0] = hist_avg[0];
+
+    for (int i = 1; i < bins; i++)
+    {
+        cum_hist[i] = cum_hist[i - 1] + hist_avg[i];
+    }
+
+    // Apply equalization
+    cudaMemcpy(d_cum_hist, cum_hist, bins * sizeof(float), cudaMemcpyHostToDevice);
+    equalizationKernel<<<gridDims, blockDims>>>(d_img, d_res, d_cum_hist, width, height);
+
+    cudaMemcpy(res, d_res, width * height * 3 * sizeof(unsigned char), cudaMemcpyDeviceToHost);
+
+    // Free device memory
+    cudaFree(d_img);
+    cudaFree(d_hist_r);
+    cudaFree(d_hist_g);
+    cudaFree(d_hist_b);
+    cudaFree(d_hist_avg);
+    cudaFree(d_cum_hist);
+}
+
 ////////////////////
 /* - Conversion - */
 ////////////////////
--- cuda/main.cpp
+++ cuda/main.cpp
@@ -13,62 +13,90 @@ using namespace cimg_library;
 void CUDARGBtoYCbCr(const unsigned char *img, unsigned char *res, int width, int height);
 void CUDAYCbCrtoRGB(const unsigned char *img, unsigned char *res, int width, int height);
 
+// Enhancement
+void CUDAHistogramEqualization(const unsigned char *img, unsigned char *res, const int width, const int height);
+
 int main()
 {
-    /////////////////////
-    /* - Parse Input - */
-    /////////////////////
+    CImg<unsigned char> inputImage("images-input/inputHequalization.bmp");
+    CImg<unsigned char> outputImage(inputImage.width(), inputImage.height(), 1, 3);
+    CUDAHistogramEqualization(inputImage, outputImage, inputImage.width(), inputImage.height());
+    outputImage.save("images-output/enhance-equalization.png");
 
-    std::string functionality;
-    std::string algorithm;
-    std::string inputPath;
-    std::string outputPath;
+    // /////////////////////
+    // /* - Parse Input - */
+    // /////////////////////
 
-    std::cout << "Enter functionality (TODO/TODO/conversion): ";
-    std::cin >> functionality;
+    // std::string functionality;
+    // std::string algorithm;
+    // std::string inputPath;
+    // std::string outputPath;
 
-    if (functionality == "conversion")
-    {
-        ;
-    }
-    else
-    {
-        std::cerr << "Invalid Functionality Selection" << std::endl;
-        return 1;
-    }
+    // std::cout << "Enter functionality (TODO/enhance/conversion): ";
+    // std::cin >> functionality;
 
-    std::cout << "Enter input image path: ";
-    std::cin >> inputPath;
+    // if (functionality == "enhance")
+    // {
+    //     std::cout << "Enter algorithm (equalization/TODO): ";
+    //     std::cin >> algorithm;
+    // }
+    // else if (functionality == "conversion")
+    // {
+    //     ;
+    // }
+    // else
+    // {
+    //     std::cerr << "Invalid Functionality Selection" << std::endl;
+    //     return 1;
+    // }
 
-    ///////////////////////
-    /* - Run Algorithm - */
-    ///////////////////////
+    // std::cout << "Enter input image path: ";
+    // std::cin >> inputPath;
 
-    CImg<unsigned char> inputImage(inputPath.c_str());
-    CImg<unsigned char> outputImage;
-    Timer MyTimer;
+    // ///////////////////////
+    // /* - Run Algorithm - */
+    // ///////////////////////
 
-    if (functionality == "conversion")
-    {
-        ////////////////////
-        /* - Conversion - */
-        ////////////////////
+    // CImg<unsigned char> inputImage(inputPath.c_str());
+    // Timer MyTimer;
 
-        // RGB to YCBCR + Grayscale
-        CImg<unsigned char> resgray(inputImage.width(), inputImage.height(), 1, 3);
-        CImg<unsigned char> resrgb(inputImage.width(), inputImage.height(), 1, 3);
-        CUDARGBtoYCbCr(inputImage, resgray, inputImage.width(), inputImage.height());
+    // if (functionality == "enhance")
+    // {
+    //     ///////////////////
+    //     /* - Enhancing - */
+    //     ///////////////////
 
-        float duration = MyTimer.elapsed();
-        std::cout << "YCBCR/Grayscale simulation time: " << duration << std::endl;
-        resgray.save("images-output/conversion-ycbcr.png");
-        resgray.get_channel(0).save("images-output/conversion-grayscale.png");
+    //     if (algorithm == "equalization")
+    //     {
+    //         CImg<unsigned char> outputImage(inputImage.width(), inputImage.height(), 1, 3);
+    //         CUDAHistogramEqualization(inputImage, outputImage, inputImage.width(), inputImage.height());
 
-        // YCBCR to RGB
-        Timer RGBSimulationTimer;
-        CUDAYCbCrtoRGB(resgray, resrgb, resgray.width(), resgray.height());
-        float rgbSimulationTime = RGBSimulationTimer.elapsed();
-        std::cout << "RGB simulation time: " << rgbSimulationTime << std::endl;
-        resrgb.save("images-output/conversion-rgb.png");
-    }
+    //         float duration = MyTimer.elapsed();
+    //         outputImage.save("images-output/enhance-equalization.png");
+    //         std::cout << "Simulation time: " << duration << std::endl;
+    //     }
+    // }
+    // else if (functionality == "conversion")
+    // {
+    //     ////////////////////
+    //     /* - Conversion - */
+    //     ////////////////////
+
+    //     // RGB to YCBCR + Grayscale
+    //     CImg<unsigned char> resgray(inputImage.width(), inputImage.height(), 1, 3);
+    //     CImg<unsigned char> resrgb(inputImage.width(), inputImage.height(), 1, 3);
+    //     CUDARGBtoYCbCr(inputImage, resgray, inputImage.width(), inputImage.height());
+
+    //     float duration = MyTimer.elapsed();
+    //     std::cout << "YCBCR/Grayscale simulation time: " << duration << std::endl;
+    //     resgray.save("images-output/conversion-ycbcr.png");
+    //     resgray.get_channel(0).save("images-output/conversion-grayscale.png");
+
+    //     // YCBCR to RGB
+    //     Timer RGBSimulationTimer;
+    //     CUDAYCbCrtoRGB(resgray, resrgb, resgray.width(), resgray.height());
+    //     float rgbSimulationTime = RGBSimulationTimer.elapsed();
+    //     std::cout << "RGB simulation time: " << rgbSimulationTime << std::endl;
+    //     resrgb.save("images-output/conversion-rgb.png");
+    // }
 }
